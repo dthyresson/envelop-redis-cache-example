b4be1ee1cad95f3bf552b2d4ae021d4a99fea0db
A. David Thyresson <dthyresson@gmail.com>
2021-10-08 20:58:56 -0400

    replace response test

--- src/edge-handlers/logAllRequests.js
+++ src/edge-handlers/logAllRequests.js
@@ -1,4 +1,8 @@
-// Log every incoming request URL
-export function onRequest(event) {
-  console.log(`Incoming request for ${event.requestMeta.url}`);
-}
+event.replaceResponse(
+  new Response(null, {
+    status: 301,
+    headers: {
+      Location: "https://www.netlify.com/products/edge/",
+    },
+  })
+);
