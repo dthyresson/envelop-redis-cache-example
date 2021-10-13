89f2468322dc0294dd408e374e5a68d63657f84e
A. David Thyresson <dthyresson@gmail.com>
2021-10-13 17:18:36 -0400

    try cacheKey

--- src/edge-handlers/logAllRequests.js
+++ src/edge-handlers/logAllRequests.js
@@ -7,16 +7,16 @@ const crypto = require('crypto')
  * It is exported here for advanced use-cases. E.g. if you want to short circuit and serve responses from the cache on a global level in order to completely by-pass the GraphQL flow.
  */
 const buildResponseCacheKey = params => crypto.createHash('sha1').update([params.documentString,
-  params.operationName ?? '',
+  // params.operationName ?? '',
   // jsonStableStringify(params.variableValues ?? {}),
   // params.sessionId ?? '',
 ].join('|')).digest('base64')
 
 export const onRequest = (event) => {
-  console.log(`incoming request for ${event.requestMeta.url.pathname}`)
+  console.info(`incoming request for ${event.requestMeta.url.pathname}`)
 
   event.replaceResponse(async ({ request }) => {
-    console.log(request.url, `url for ${event.requestMeta.url.pathname}`)
+    console.info(request.url, `url for ${event.requestMeta.url.pathname}`)
 
     const rawBody = request.body
     const decoder = new TextDecoder()
@@ -27,10 +27,10 @@ export const onRequest = (event) => {
     while (read) {
       const chunk = await reader.read()
 
-      console.log(chunk, `chunk for ${event.requestMeta.url.pathname}`)
+      console.debug(chunk, `chunk for ${event.requestMeta.url.pathname}`)
 
       if (!chunk.done) {
-        console.log('trying to decode...')
+        console.debug('trying to decode...')
         body += decoder.decode(chunk.value)
       } else {
         read = false
@@ -62,13 +62,16 @@ export const onRequest = (event) => {
       query: event.requestMeta.url,
     }
 
-    console.log(payload, `payload for ${event.requestMeta.url.pathname}`)
+    console.debug(payload, `payload for ${event.requestMeta.url.pathname}`)
 
-    console.log({ params }, `GraphQL Parameters for ${event.requestMeta.url.pathname}`)
+    console.debug({ params }, `GraphQL Parameters for ${event.requestMeta.url.pathname}`)
 
-    const cacheKey = buildResponseCacheKey(params)
-
-    console.log(cacheKey, `cacheKey for ${event.requestMeta.url.pathname}`)
+    try {
+      const cacheKey = buildResponseCacheKey(params)
+      console.debug(cacheKey, `cacheKey for ${event.requestMeta.url.pathname}`)
+    } catch (error) {
+      console.error(error)
+    }
 
     return fetch(url, { body: payload.body, headers: payload.headers, method: 'POST' })
   })
