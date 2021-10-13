d08e77e3907ed0b1b7cfb8829c45611d0359f75f
A. David Thyresson <dthyresson@gmail.com>
2021-10-13 16:46:17 -0400

    getGraphQLParameters

--- src/edge-handlers/logAllRequests.js
+++ src/edge-handlers/logAllRequests.js
@@ -1,5 +1,4 @@
-// import { envelop } from '@envelop/core'
-// import { getGraphQLParameters } from 'graphql-helix'
+import { getGraphQLParameters } from 'graphql-helix'
 
 // import jsonStableStringify from 'fast-json-stable-stringify'
 
@@ -26,6 +25,7 @@ export const onRequest = (event) => {
     let read = true
     let body = ''
     const reader = rawBody.getReader()
+
     while (read) {
       const chunk = await reader.read()
 
@@ -49,21 +49,13 @@ export const onRequest = (event) => {
       method: event.requestMeta.method,
       query: event.requestMeta.url,
     }
+
     console.log(payload, `payload for ${event.requestMeta.url.pathname}`)
 
-    // url.pathname = `/api/v1/${url.pathname}`
+    const { operationName, query, variables } = getGraphQLParameters(payload)
+
+    console.log({ operationName, query, variables }, `getGraphQLParameters for ${event.requestMeta.url.pathname}`)
+
     return fetch(url, { body: payload.body, headers: payload.headers, method: 'POST' })
   })
-
-  // event.replaceResponse({ request } => {
-  //   const payload = {
-  //     body: request?.body,
-  //     headers: event.requestMeta.headers,
-  //     method: event.requestMeta.method,
-  //     query: event.requestMeta.url,
-  //   }
-  //   console.log(payload, `payload for ${event.requestMeta.url.pathname}`)
-
-  //   return fetch(request)
-  // }})
 }
