25619a064e380668eb04a6922460fa9586590524
A. David Thyresson <dthyresson@gmail.com>
2021-10-13 10:45:24 -0400

    just log event info

--- src/edge-handlers/logAllRequests.js
+++ src/edge-handlers/logAllRequests.js
@@ -1,20 +1,20 @@
 import jsonStableStringify from 'fast-json-stable-stringify'
 
-const crypto = require('crypto')
+// const crypto = require('crypto')
 
 /**
  * Default function used for building the response cache key.
  * It is exported here for advanced use-cases. E.g. if you want to short circuit and serve responses from the cache on a global level in order to completely by-pass the GraphQL flow.
  */
-const buildResponseCacheKey = params => crypto.createHash('sha1').update([params.documentString,
-  params.operationName ?? '',
-  jsonStableStringify(params.variableValues ?? {}),
-  params.sessionId ?? '',
-].join('|')).digest('base64')
+// const buildResponseCacheKey = params => crypto.createHash('sha1').update([params.documentString,
+//   params.operationName ?? '',
+//   jsonStableStringify(params.variableValues ?? {}),
+//   params.sessionId ?? '',
+// ].join('|')).digest('base64')
 
 export const onRequest = (event) => {
   console.log(`incoming request for ${event.requestMeta.url.pathname}`)
   console.log(jsonStableStringify(event), `incoming event for ${event.requestMeta.url.pathname}`)
   console.log(jsonStableStringify(event.context), `incoming event.context for ${event.requestMeta.url.pathname}`)
-  console.log(buildResponseCacheKey(event), `buildResponseCacheKey for ${event.requestMeta.url.pathname}`)
+  // console.log(buildResponseCacheKey(event), `buildResponseCacheKey for ${event.requestMeta.url.pathname}`)
 }
